f48b1d52949d826f56c7c342ed5ea516775de21f
Дмитрий <ze322@mail.ru>
2016-05-24 09:29:18 +0600

    Added other

--- main.c
+++ main.c
@@ -8,4 +8,8 @@ int main ()
 	scanf("%d ", &b);
 	scanf("%d ", &c);
 	printf("%d*x^2+%d*x+%d", a, b, c);
+	d=b*b-4*a*c;
+	x1=((d*(1/2))+(-b))/(2*a);
+	x2=((d*(1/2))-(-b))/(2*a);
+	printf("Otvet: x1=%d; x2%d", x1, x2);
 }
